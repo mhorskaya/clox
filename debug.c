68d935b555f02e0afafb014a58d8c9501532bcac
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-06-01 06:31:31 +0300

    Fix C11 standard compilation error.

--- debug.c
+++ debug.c
@@ -11,6 +11,19 @@ void disassembleChunk(Chunk *chunk, const char *name) {
     }
 }
 
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
+
+static int simpleInstruction(const char *name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
 int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
     if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
@@ -31,15 +44,3 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     }
 }
 
-static int constantInstruction(const char *name, Chunk *chunk, int offset) {
-    uint8_t constant = chunk->code[offset + 1];
-    printf("%-16s %4d '", name, constant);
-    printValue(chunk->constants.values[constant]);
-    printf("'\n");
-    return offset + 2;
-}
-
-static int simpleInstruction(const char *name, int offset) {
-    printf("%s\n", name);
-    return offset + 1;
-}
