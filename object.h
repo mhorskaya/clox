f9148f62216a7d64c74ff0e2d1d1ef801dfe27a3
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-07-21 16:49:27 +0300

    Add classes.

--- object.h
+++ object.h
@@ -7,11 +7,13 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_CLASS(value) isObjType(value, OBJ_CLASS)
 #define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_CLASS(value) ((ObjClass*)AS_OBJ(value))
 #define AS_CLOSURE(value) ((ObjClosure*)AS_OBJ(value))
 #define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
 #define AS_NATIVE(value) (((ObjNative*)AS_OBJ(value))->function)
@@ -19,6 +21,7 @@
 #define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_CLASS,
     OBJ_CLOSURE,
     OBJ_FUNCTION,
     OBJ_NATIVE,
@@ -63,10 +66,17 @@ typedef struct sUpvalue {
 
 typedef struct {
     Obj obj;
+    ObjString *name;
+} ObjClass;
+
+typedef struct {
+    Obj obj;
     ObjFunction *function;
     ObjUpvalue **upvalues;
     int upvalueCount;
 } ObjClosure;
+
+ObjClass *newClass(ObjString *name);
 
 ObjClosure *newClosure(ObjFunction *function);
 
