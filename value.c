a8645dd2d917a817f8e46680791685ecabf05703
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-07-23 10:04:10 +0300

    Implement 'NaN boxing'.

--- value.c
+++ value.c
@@ -28,6 +28,17 @@ void freeValueArray(ValueArray *array) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+    if (IS_BOOL(value)) {
+        printf(AS_BOOL(value) ? "true" : "false");
+    } else if (IS_NIL(value)) {
+        printf("nil");
+    } else if (IS_NUMBER(value)) {
+        printf("%g", AS_NUMBER(value));
+    } else if (IS_OBJ(value)) {
+        printObject(value);
+    }
+#else
     switch (value.type) {
         case VAL_BOOL:
             printf(AS_BOOL(value) ? "true" : "false");
@@ -42,9 +53,14 @@ void printValue(Value value) {
             printObject(value);
             break;
     }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+    if (IS_NUMBER(a) && IS_NUMBER(b)) return AS_NUMBER(a) == AS_NUMBER(b);
+    return a == b;
+#else
     if (a.type != b.type) return false;
 
     switch (a.type) {
@@ -57,4 +73,5 @@ bool valuesEqual(Value a, Value b) {
         case VAL_OBJ:
             return AS_OBJ(a) == AS_OBJ(b);
     }
+#endif
 }
